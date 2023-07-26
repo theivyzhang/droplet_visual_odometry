6bf8d4945cf5a8f925380af83a97e2a0eff17a25
theivyzhang <aiwei.zhang.25@dartmouth.edu>
2023-07-26 18:31:55 -0400

    included preliminary results for trajectory eval on some 20 frames; other changes

--- scripts/trajectory_evaluation_ground_truth.py
+++ scripts/trajectory_evaluation_ground_truth.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 # Author: Ivy Aiwei Zhang
-# Last updated: 7-24-2023
-# Purpose: correct way to extract ground truth and convert into good frames
+# Last updated: 7-26-2023
+# Purpose: this is a looped ground truth extraction where the camera to marker translation and quaternion representation of rotation is extracted,
+# then stored in a txt file in the correct format
 
 # ROS node messages
 print("extracting ground truth from two consecutive frames and giving us the marker to marker transformations")
@@ -31,43 +32,45 @@ DEFAULT_CAMERA_TOPIC = '/cam_0_optical_frame'
 class FrameExtraction:
     def __init__(self, default_base_link_topic=DEFAULT_BASE_LINK_TOPIC, default_camera_topic=DEFAULT_CAMERA_TOPIC, starting_index=1, loop=False):
 
+        # print("quaternion matrix of homogenous transformations matrix [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [0, 0,0,1]] is {}".
+        #       format(self.quaternion_representation(np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [0, 0,0,1]]))))
         """
         by initializing vo, the image subscriber in hypothesis is activated
         uncomment the draw matches section that save the matched image
         """
-
+        self.frame_one = None
+        self.image_one = None
+        self.frame_two = None
+        self.image_two = None
         self.robot_frame_2_stamp = None
         self.robot_frame_1_stamp = None
 
         self.bridge = CvBridge()
         self.parse_camera_intrinsics()
 
-        self.frame_one = None
-        self.image_one = None
-        self.frame_two = None
-        self.image_two = None
-
         self.ground_truth_full_list_in_base_link = []
         self.ground_truth_list_cam_to_marker = []
 
+        # set up the needed flags
         self.done_extracting = False
-
         self.ground_truth_frame_one = False
         self.ground_truth_frame_two = False
-        self.ground_truth_index = 0
         self.detected_marker = False
-        # self.vo = VisualOdometry()
         self.frame_dimensions = (1400, 1080)
 
         self.listener = tf.TransformListener()
         self.default_base_link_topic = default_base_link_topic
         self.default_camera_topic = default_camera_topic
+
         self.starting_index = starting_index
         self.loop = loop
+        self.file_name = "/home/ivyz/Documents/ivy_workspace/src/vis_odom/scripts/stamped_ground_truth.txt"
 
 
         # finally, activate all the callbacks
         self.activate_callbacks()
+
+
 
 
     def activate_callbacks(self):
@@ -170,73 +173,124 @@ class FrameExtraction:
     """
     this method returns the quaternion representation of a rotation matrix
     """
+
+    def rotation_matrix_to_quaternion(self, rotation_matrix):
+        trace = np.trace(rotation_matrix)
+        if trace > 0:
+            S = np.sqrt(trace + 1.0) * 2.0
+            w = 0.25 * S
+            x = (rotation_matrix[2, 1] - rotation_matrix[1, 2]) / S
+            y = (rotation_matrix[0, 2] - rotation_matrix[2, 0]) / S
+            z = (rotation_matrix[1, 0] - rotation_matrix[0, 1]) / S
+        elif (rotation_matrix[0, 0] > rotation_matrix[1, 1]) and (rotation_matrix[0, 0] > rotation_matrix[2, 2]):
+            S = np.sqrt(1.0 + rotation_matrix[0, 0] - rotation_matrix[1, 1] - rotation_matrix[2, 2]) * 2.0
+            w = (rotation_matrix[2, 1] - rotation_matrix[1, 2]) / S
+            x = 0.25 * S
+            y = (rotation_matrix[0, 1] + rotation_matrix[1, 0]) / S
+            z = (rotation_matrix[0, 2] + rotation_matrix[2, 0]) / S
+        elif rotation_matrix[1, 1] > rotation_matrix[2, 2]:
+            S = np.sqrt(1.0 + rotation_matrix[1, 1] - rotation_matrix[0, 0] - rotation_matrix[2, 2]) * 2.0
+            w = (rotation_matrix[0, 2] - rotation_matrix[2, 0]) / S
+            x = (rotation_matrix[0, 1] + rotation_matrix[1, 0]) / S
+            y = 0.25 * S
+            z = (rotation_matrix[1, 2] + rotation_matrix[2, 1]) / S
+        else:
+            S = np.sqrt(1.0 + rotation_matrix[2, 2] - rotation_matrix[0, 0] - rotation_matrix[1, 1]) * 2.0
+            w = (rotation_matrix[1, 0] - rotation_matrix[0, 1]) / S
+            x = (rotation_matrix[0, 2] + rotation_matrix[2, 0]) / S
+            y = (rotation_matrix[1, 2] + rotation_matrix[2, 1]) / S
+            z = 0.25 * S
+
+        return [x, y, z, w]
+
     def quaternion_representation(self, homogenous_matrix):
         rotation_matrix = homogenous_matrix[:3, :3]
-        print("here is the rotation matrix: {}".format(rotation_matrix))
-        quaternion_rep = tf.quaternion_from_matrix(rotation_matrix)
-        print("here is the quaternion representation: {}".format(quaternion_rep))
+        # print("here is the rotation matrix: {}".format(rotation_matrix))
+        quaternion_rep =self.rotation_matrix_to_quaternion(rotation_matrix)
+        # print("here is the quaternion representation: {}".format(quaternion_rep))
         return quaternion_rep
 
-
-
     """
-    this method computes the camera to marker translation (ground truth) at a given frame
+    these methods computes the camera to marker translation (ground truth) at a given frame
     """
-
-    def compute_frame_camera_to_marker(self, markers):
-        # TODO:
-        # PART A:
-        # for each frame, first get the pose translation and rotation info from base link
-        # use tf transformations to get translation and rotation matrices
-        # concatenate the two; the output is a homogenous transformation matrix 4x4, and yon now have base to marker
-
+    def get_base_to_marker_homogenous_transformation(self, markers):
         bTm_translation = markers[0].pose.pose.position
         btm_orientation = markers[0].pose.pose.orientation
-        print("we have btm translation {} and orientation {}".format(bTm_translation, btm_orientation))
+        # print("we have btm translation {} and orientation {}".format(bTm_translation, btm_orientation))
 
         bTm_translation_array = np.array([bTm_translation.x, bTm_translation.y, bTm_translation.z])
         bTm_rotation_array = np.array([btm_orientation.x, btm_orientation.y, btm_orientation.z, btm_orientation.w])
 
         bTm_translation_mat = tf.transformations.translation_matrix(bTm_translation_array)
         bTm_orientation_mat = tf.transformations.quaternion_matrix(bTm_rotation_array)
-        print("we have btm translation matrix {} and orientation matrix {}".format(bTm_translation, btm_orientation))
+        # print("we have btm translation matrix {} and orientation matrix {}".format(bTm_translation, btm_orientation))
 
         btm_homogenous_translation_mat = tf.transformations.concatenate_matrices(bTm_translation_mat,
                                                                                  bTm_orientation_mat)
-        print("we homogenous translation matrix {}".format(btm_homogenous_translation_mat))
+        return btm_homogenous_translation_mat
+
+    def get_camera_to_base_homogenous_transformation_matrix(self, markers):
+        cTb_translation, cTb_rotation = self.listener.lookupTransform(self.default_base_link_topic,
+                                                                      self.default_camera_topic,
+                                                                      markers[0].header.stamp)
+        # print("we have cTb translation {} and orientation {}".format(cTb_translation, cTb_rotation))
+
+        cTb_translation_mat = tf.transformations.translation_matrix(cTb_translation)
+        cTb_orientation_mat = tf.transformations.quaternion_matrix(cTb_rotation)
+        # print("we have cTb translation matrix {} and orientation matrix {}".format(cTb_translation_mat,
+        #                                                                            cTb_orientation_mat))
+
+        cTb_homogenous_translation_mat = tf.transformations.concatenate_matrices(cTb_translation_mat,
+                                                                                 cTb_orientation_mat)
+        return cTb_homogenous_translation_mat
+
+    def compute_frame_camera_to_marker(self, markers):
+        time_stamp = markers[0].header.stamp # first you get the timestamp
+
+        # TODO:
+        # PART A:
+        # for each frame, first get the pose translation and rotation info from base link
+        # use tf transformations to get translation and rotation matrices
+        # concatenate the two; the output is a homogenous transformation matrix 4x4, and yon now have base to marker
+        btm_homogenous_translation_mat = self.get_base_to_marker_homogenous_transformation(markers)
+        # print("we have the base to marker homogenous translation matrix {}".format(btm_homogenous_translation_mat))
 
         # PART B:
         # to get camera to baselink:
         # lookUpTransform produces a cam-2-baselink translation + rotation; repeat steps 2 - 3 in part A; produces 4x4 CTB
         # dot product: CTB dot BTM, you get CTM which is what the output of this function should be
+        cTb_homogenous_translation_mat = self.get_camera_to_base_homogenous_transformation_matrix(markers)
 
-        cTb_translation, cTb_rotation = self.listener.lookupTransform(self.default_base_link_topic,
-                                                                      self.default_camera_topic,
-                                                                      markers[0].header.stamp)
-        print("we have cTb translation {} and orientation {}".format(cTb_translation, cTb_rotation))
-
-        cTb_translation_mat = tf.transformations.translation_matrix(cTb_translation)
-        cTb_orientation_mat = tf.transformations.quaternion_matrix(cTb_rotation)
-        print("we have cTb translation matrix {} and orientation matrix {}".format(cTb_translation_mat,
-                                                                                   cTb_orientation_mat))
-
-        cTb_homogenous_translation_mat = tf.transformations.concatenate_matrices(cTb_translation_mat,
-                                                                                 cTb_orientation_mat)
-        print("we homogenous translation matrix {}".format(cTb_homogenous_translation_mat))
+        # print("we have the camera to base homogenous translation matrix {}".format(cTb_homogenous_translation_mat))
 
         # now get camera to marker transformation (4x4 homogenous transformation matrix)
         cam_to_marker_transformation = np.matmul(btm_homogenous_translation_mat, cTb_homogenous_translation_mat)
 
-        print("Here is the camera to marker transformation: {}".format(cam_to_marker_transformation))
+        # print("Here is the camera to marker transformation: {}".format(cam_to_marker_transformation))
         self.ground_truth_list_cam_to_marker.append(cam_to_marker_transformation)
 
         """""
         Now prepare the data needed for trajectory evaluation
         1) extract rotation from cam_to_marker translation, turn into quaternion representation with x, y, z, w
         """
-        self.quaternion_representation(cam_to_marker_transformation)
+        cam_to_marker_quaternion = self.quaternion_representation(cam_to_marker_transformation)
+        print("here is the camera_to_marker_quaternion: {}".format(cam_to_marker_quaternion))
+        cam_to_marker_translation = [cam_to_marker_transformation[0, 3], cam_to_marker_transformation[1, 3], cam_to_marker_transformation[2, 3]]
+        print("here is the camera_to_marker_translation: {}".format(cam_to_marker_translation))
 
+        # write the information needed for the trajectory evaluation
+        with open(self.file_name, 'a') as file:
+            file.write(str(time_stamp)+" "+str(cam_to_marker_translation[0])+" "+str(cam_to_marker_translation[1])+" "+str(cam_to_marker_translation[2])+" "
+                           +str(cam_to_marker_quaternion[0])+" "+str(cam_to_marker_quaternion[1])+" "+str(cam_to_marker_quaternion[2])+" "+str(cam_to_marker_quaternion[3])+"\n")
         return cam_to_marker_transformation
+
+
+    # def write_to_ground_truth_file(self, file_name, timestamp, translation, quaternion):
+    #     try:
+    #         with open(file_name, 'a') as file
+    #
+    #     except:
+    #         print("file is not found")
 
     """
     this method gets the marker to marker translation every two consecutive frames with marker readings
@@ -277,15 +331,11 @@ class FrameExtraction:
                             frame2_cam2marker = self.compute_frame_camera_to_marker(markers)
                             # self.compute_and_save_reading(markers)
 
-                        if len(self.ground_truth_list_cam_to_marker) == 2:
+                        if len(self.ground_truth_list_cam_to_marker) == 2 and not self.loop:
                             self.get_translation_between_two_frames(frame1_cam2marker, frame2_cam2marker)
-                            if not self.loop:
-                                self.ground_truth_subscriber.unregister()
+                            self.ground_truth_subscriber.unregister()
                     else:
                         print("did not find marker 0")
-
-
-
 
             else:
                 rospy.debug("No markers detected")
